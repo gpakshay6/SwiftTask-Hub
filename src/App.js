304dfa188b5f6a6ba6296309efa9ea2e71567fce
gpakshay6 <gowda.akshay27@gmail.com>
2024-02-12 19:03:42 +0530

    app.js changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Container, Toolbar, CssBaseline } from '@mui/material';
 import Login from './pages/Login/Login';
 import Register from './pages/Signup/Register';
 import Forget from './pages/Login/Forget';
@@ -9,6 +10,7 @@ import Activity from './pages/Dashboard/Activity';
 import Tasks from './pages/Dashboard/Tasks';
 import Projects from './pages/Dashboard/Projects';
 import NoPage from './pages/NoPage';
+import { Header } from './components';
 
 const App = () => {
   const defaultTheme = createTheme();
@@ -16,21 +18,39 @@ const App = () => {
   return (
     <BrowserRouter>
       <ThemeProvider theme={defaultTheme}>
+        <CssBaseline />
         <Routes>
-          <Route path="/">
-            <Route index element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="forgot" element={<Forget />} />
-            <Route path="home" element={<Home />} />
-            <Route path="activity" element={<Activity />} />
-            <Route path="tasks" element={<Tasks />} />
-            <Route path="projects" element={<Projects />} />
-            <Route path="*" element={<NoPage />} />
-          </Route>
+          <Route path="/" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="forgot" element={<Forget />} />
+          <Route path="home" element={<PrivateRoute component={<Home />} />} />
+          <Route
+            path="activity"
+            element={<PrivateRoute component={<Activity />} />}
+          />
+          <Route
+            path="tasks"
+            element={<PrivateRoute component={<Tasks />} />}
+          />
+          <Route
+            path="projects"
+            element={<PrivateRoute component={<Projects />} />}
+          />
+          <Route path="*" element={<NoPage />} />
         </Routes>
       </ThemeProvider>
     </BrowserRouter>
   );
 };
+
+const PrivateRoute = ({ component }) => (
+  <>
+    <Toolbar />
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <Header />
+      {component}
+    </Container>
+  </>
+);
 
 export default App;
--- src/pages/Dashboard/Home.js
+++ src/pages/Dashboard/Home.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Container, Toolbar, CssBaseline, Grid, Paper } from '@mui/material';
+import { Grid, Paper } from '@mui/material';
 import {
   StatsCard,
   TaskCard,
   ProjectCard,
   ActivityCard,
-  Header,
 } from '../../components';
 import {
   recentActivities,
@@ -17,63 +16,58 @@ import {
 const Home = () => {
   return (
     <>
-      <CssBaseline />
-      <Toolbar />
-      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-        <Header />
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={8} lg={9}>
-            <Paper
-              sx={{
-                p: 2,
-                boxShadow:
-                  'rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset',
-              }}
-            >
-              <ActivityCard
-                label={'Recent Activity'}
-                activities={recentActivities}
-              />
-            </Paper>
-          </Grid>
-          <Grid item xs={12} md={4} lg={3}>
-            <Paper
-              sx={{
-                p: 2,
-                boxShadow:
-                  'rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset',
-              }}
-            >
-              <StatsCard label={'Quick Stats'} stats={quickStats} />
-            </Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper
-              sx={{
-                p: 2,
-                boxShadow:
-                  'rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset',
-              }}
-            >
-              <TaskCard label={'Tasks'} task={tasks} />
-            </Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper
-              sx={{
-                p: 2,
-                boxShadow:
-                  'rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset',
-              }}
-            >
-              <ProjectCard
-                label={'Project Overview'}
-                projects={projectOverview}
-              />
-            </Paper>
-          </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={8} lg={9}>
+          <Paper
+            sx={{
+              p: 2,
+              boxShadow:
+                'rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset',
+            }}
+          >
+            <ActivityCard
+              label={'Recent Activity'}
+              activities={recentActivities}
+            />
+          </Paper>
         </Grid>
-      </Container>
+        <Grid item xs={12} md={4} lg={3}>
+          <Paper
+            sx={{
+              p: 2,
+              boxShadow:
+                'rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset',
+            }}
+          >
+            <StatsCard label={'Quick Stats'} stats={quickStats} />
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper
+            sx={{
+              p: 2,
+              boxShadow:
+                'rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset',
+            }}
+          >
+            <TaskCard label={'Tasks'} task={tasks} />
+          </Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper
+            sx={{
+              p: 2,
+              boxShadow:
+                'rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset',
+            }}
+          >
+            <ProjectCard
+              label={'Project Overview'}
+              projects={projectOverview}
+            />
+          </Paper>
+        </Grid>
+      </Grid>
     </>
   );
 };
